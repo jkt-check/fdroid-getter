049ae9a8e15d934a6a9d666f6d28a12a0678e397
jkt-check <jacktian16898@gmail.com>
2022-06-05 23:12:40 +0800

    add upload-all capbility

--- generateAppDefinition.go
+++ generateAppDefinition.go
@@ -12,6 +12,8 @@ import (
 type CmdArgs struct {
 	pkgname string // "-p"
 	action  string // "-a"
+	force   bool   // "-f" , when ScreenShotUrls is empty, i also want to create it
+	//	listCategories bool   // "-lc"
 }
 
 var cmdArgs = CmdArgs{}
@@ -25,6 +27,8 @@ func init() {
 
 	generateCommand.Fset.StringVar(&cmdArgs.pkgname, "p", "", "the android app's pkgname")
 	generateCommand.Fset.StringVar(&cmdArgs.action, "a", "", "action for appdefinition")
+	generateCommand.Fset.BoolVar(&cmdArgs.force, "f", false, "force create appdefinition even though ScreenShotUrls nonexist")
+	//	generateCommand.Fset.BoolVar(&cmdArgs.listCategories, "lc", false, "listCategories")
 	commands = append(commands, generateCommand)
 }
 
@@ -54,9 +58,8 @@ type AppDefinitionConfig struct {
 	Endpoint string
 }
 
-var adconfig = AppDefinitionConfig{Token: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTIyNzI3MzMsImlzcyI6ImxlYWZzYW5kIiwicnNjIjoidXNlci1hY2NvdW50Iiwic3ViIjoiMDFjNjhkOTAtNWZhNC00Zjc1LWI0N2QtNTAzYzk1ZmQ1OGNkIiwidHlwIjoic2lnbmluIn0.q24vZ34AH1t-rKoc2cHMHXJ6wqmvcUO2Uxk8cgy1i_8KIjKID4Fi87ewXxyBmCs09gudzGn74SZQUZ7Ig1b2TJFgupEEyve-f99-2ZN_AzfDqLth7lee-iafZIbNRZipLKFIXb1Kbn7TfzzY0mzgOAvnmG9madbTh4pmMRHgaDc",
-	Endpoint: "http://13.230.247.221:8080/app/v1/definitions"}
-
+var adconfig = AppDefinitionConfig{Token: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTQ0ODE4OTcsImlzcyI6ImxlYWZzYW5kIiwicnNjIjoidXNlci1hY2NvdW50Iiwic3ViIjoiMDFjNjhkOTAtNWZhNC00Zjc1LWI0N2QtNTAzYzk1ZmQ1OGNkIiwidHlwIjoic2lnbmluIn0.hVGTdU1N4mA0OzACyCv9hKscuSeN-lyQjVElGK3ZQhgbeFx76d11MUvjuTj4_5Cao-x5KgsEcUvZ4Cvg0iAKgueuRD4uInIVaLJL4GZzJ22VDaz6Kn0-8BRzHyW-J-UmVW3N2m7B4cs-bNHcZb8hQvB_e_s49EhYmjYrC3B7qb8",
+	Endpoint: "https://api.teaco.io/app/v1/definitions"}
 var (
 	generateCommand *Command
 	pkgName         string
@@ -70,14 +73,20 @@ func generateAppDefinition(args []string) error {
 		return err
 	}
 
-	if cmdArgs.pkgname == "" {
+	if cmdArgs.pkgname == "" && cmdArgs.action != "upload-all" {
 		fmt.Fprintf(os.Stderr, " pls input the pkgName\n")
 		return fmt.Errorf("pls input pkgName")
 	}
 
+	uploadItem, totalItem := 0, 0
+	defer func() {
+		fmt.Printf("TotalItem: [%d], UploadItem: [%d]\n", totalItem, uploadItem)
+	}()
 	for _, app := range apps {
+		totalItem++
 
-		if app.PackageName == cmdArgs.pkgname {
+		if cmdArgs.action == "upload-all" {
+
 			adi := AppDefinitionInput{
 				Platform:            "android",
 				Logo:                app.IconURL2(),
@@ -92,54 +101,155 @@ func generateAppDefinition(args []string) error {
 				PkgType:             "apk",
 				Developer:           app.AuthorName,
 			}
-
-			if cmdArgs.action == "show-app-info" {
-				appinfo, err := json.Marshal(&app)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "json parse error, [%v]\n", err)
-				} else {
-					fmt.Fprintf(os.Stderr, "%s\n", string(appinfo))
-				}
-				return nil
-
+			if len(adi.ScreenShotUrls) > 4 {
+				adi.ScreenShotUrls = adi.ScreenShotUrls[0:4]
 			}
+			_, err := checkPackage(&adi)
 
-			if cmdArgs.action == "show-adi-info" {
+			if err != nil {
 
-				if adiinfo, err := json.Marshal(&adi); err != nil {
-					fmt.Fprintf(os.Stderr, "json parse error, [%v]\n", err)
-				} else {
-					fmt.Fprintf(os.Stderr, "%s\n", string(adiinfo))
-				}
-				return nil
-
+				fmt.Println("json parse error:", err)
+				//return err
+				continue
 			}
-
 			adiJson := []byte{}
-			adiJson, err := json.Marshal(&adi)
+			adiJson, err = json.Marshal(&adi)
 			if err != nil {
 				fmt.Println("json parse error:", err)
-				return err
+				//return err
+				continue
 			}
 			//			fmt.Fprintf(os.Stderr, "adi :\n%s", string(adiJson))
 
 			responseData, status, err := PostTeacoRequest(adiJson)
 			if err != nil {
-				return err
+				//return err
+				fmt.Println("PostTeacoRequest error")
+				continue
 			}
 			if status > 400 {
-				return fmt.Errorf("PostTeacoRequest error with status code: %d, with data: %s ", status, string(responseData))
+				//return fmt.Errorf("PostTeacoRequest error with status code: %d, with data: %s ", status, string(responseData))
+				fmt.Printf("PostTeacoRequest error with status code: %d, with data: %s ", status, string(responseData))
+				continue
 			}
 			fmt.Println("response data:")
 			fmt.Println(string(responseData))
+			uploadItem++
+			/*
+				if uploadItem > 10 {
+					break
+				}
+			*/
 
-			return nil
+		} else {
+
+			if cmdArgs.pkgname != "" {
+				if app.PackageName == cmdArgs.pkgname {
+					adi := AppDefinitionInput{
+						Platform:            "android",
+						Logo:                app.IconURL2(),
+						PkgUrl:              app.Apks[0].URL(),
+						Visibility:          "public",
+						DetailedDescription: app.Description,
+						SourceCodeUrl:       app.SourceCode,
+						Category:            app.Categories[0],
+						ShortDescription:    app.Summary,
+						ScreenShotUrls:      app.ScreenshotURLs(),
+						PkgMd5:              "-",
+						PkgType:             "apk",
+						Developer:           app.AuthorName,
+					}
+
+					if len(adi.ScreenShotUrls) > 4 {
+						adi.ScreenShotUrls = adi.ScreenShotUrls[0:4]
+					}
+
+					if cmdArgs.action == "show-app-info" {
+						appinfo, err := json.Marshal(&app)
+						if err != nil {
+							fmt.Fprintf(os.Stderr, "json parse error, [%v]\n", err)
+						} else {
+							fmt.Fprintf(os.Stderr, "%s\n", string(appinfo))
+						}
+						return nil
+
+					}
+
+					if cmdArgs.action == "show-adi-info" {
+
+						if adiinfo, err := json.Marshal(&adi); err != nil {
+							fmt.Fprintf(os.Stderr, "json parse error, [%v]\n", err)
+						} else {
+							fmt.Fprintf(os.Stderr, "%s\n", string(adiinfo))
+						}
+						return nil
+
+					}
+
+					_, err := checkPackage(&adi)
+
+					if err != nil {
+
+						fmt.Println("json parse error:", err)
+						return err
+					}
+					adiJson := []byte{}
+					adiJson, err = json.Marshal(&adi)
+					if err != nil {
+						fmt.Println("json parse error:", err)
+						return err
+					}
+					//			fmt.Fprintf(os.Stderr, "adi :\n%s", string(adiJson))
+
+					responseData, status, err := PostTeacoRequest(adiJson)
+					if err != nil {
+						return err
+					}
+					if status > 400 {
+						return fmt.Errorf("PostTeacoRequest error with status code: %d, with data: %s ", status, string(responseData))
+					}
+					fmt.Println("response data:")
+					fmt.Println(string(responseData))
+
+					uploadItem++
+					return nil
+				}
+			}
 		}
 
 	}
-	fmt.Fprintln(os.Stderr, "no pkg found")
+	if cmdArgs.pkgname != "" {
+		fmt.Fprintln(os.Stderr, "no pkg found")
+	}
 	return nil
 
+}
+
+func checkPackage(adi *AppDefinitionInput) (bool, error) {
+
+	if adi.Logo == "" {
+		return false, fmt.Errorf("logoURL is nil")
+	}
+	if len(adi.Logo) < 8 {
+		return false, fmt.Errorf("logoURL is not legal [%s]", adi.Logo)
+	}
+	if adi.Logo[:7] != "http://" && adi.Logo[:8] != "https://" {
+		return false, fmt.Errorf("logoURL is not legal [%s], must start with [http://] or [https://]", adi.Logo)
+	}
+	/*
+		if len(adi.ScreenShotUrls) == 0 {
+			if !cmdArgs.force {
+				fmt.Println(cmdArgs.pkgname, ":\n", adi.DetailedDescription)
+				return false, fmt.Errorf("ScreenShotUrls Is Empty!!")
+			}
+		}
+	*/
+
+	if len(adi.ShortDescription) == 0 {
+		return false, fmt.Errorf("app ShortDescription is empty!!")
+	}
+
+	return true, nil
 }
 func PostTeacoRequest(rd []byte) ([]byte, int, error) {
 
